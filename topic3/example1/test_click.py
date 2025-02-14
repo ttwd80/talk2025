2ccc64c7b17c1b8e203d2ba1ee1909180b392edf
Titi Wangsa Damhore <111611+ttwd80@users.noreply.github.com>
2025-02-14 07:02:03 -0800

    Update test_click.py

--- topic3/example1/test_click.py
+++ topic3/example1/test_click.py
@@ -1,16 +1,27 @@
 import pytest
-from topic3.example1.click import get_hyperlinks    
+from topic3.example1.click import get_hyperlinks
 
 
-def test_click_good():
+def test_click_good_one():
+    # test valid page with one link
     links = get_hyperlinks("http://example.com")
     assert links == ["https://www.iana.org/domains/example"]
 
+
+def test_click_good_different():
+    # test valid page with one link
+    links = get_hyperlinks("https://www.q.wa-k12.net/lkwashSTS")
+    assert links == ["/lkwashSTS/Account/ForgotPassword"]
+
+
 def test_click_not_ound():
+    # test valid domain, 404 error
     links = get_hyperlinks("https://www.google.com/not-found.html")
     assert links == []
 
-def test_click_not_ound():
+
+def test_click_bad_domain():
+    # test bad domain
     # Typo on purpose
-    links = get_hyperlinks("https://uitm.edu.ny")
+    links = get_hyperlinks("https://not-uitm.ecu.ny")
     assert links == []
